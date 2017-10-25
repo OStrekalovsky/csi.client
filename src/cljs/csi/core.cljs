f60d47f41bd3fa50b63235d6c9600f056eb496ac
ostrekalovsky <strekalovsky.o.a@smilart.com>
2017-10-25 17:45:21 +0300

    Add optional parameter `info` (maybe used for auth tokens)

--- src/cljs/csi/core.cljs
+++ src/cljs/csi/core.cljs
@@ -13,66 +13,89 @@
   (close! [_self])
   (send! [_ pid message])
   (call* [_ func params])
-  (self  [_]))
+  (ext-call* [_ func params info])
+  (self [_]))
 
 (defn erlang-mbox* [socket {:keys [self] :as params}]
   (log/debug (str "creating mbox with params: " params))
 
   (let [messages (async/chan) replies (async/chan) replies-mult (async/mult replies) correlation (atom 0)]
     (go-loop []
-      (when-let [message (:message (<! socket))]
-        (let [[type body] (etf/decode message)]
-          (case type
-            :message (>! messages body)
-            :reply (>! replies body)
-            (do
-              (log/error (str "closing socket: got message of unexpected type " type))
-              (async/close! socket)))
-          (recur)))
-      (log/debug "web socket closed")
-      (async/close! messages)
-      (async/close! replies))
+             (when-let [message (:message (<! socket))]
+               (let [[type body] (etf/decode message)]
+                 (case type
+                   :message (>! messages body)
+                   :reply (>! replies body)
+                   (do
+                     (log/error (str "closing socket: got message of unexpected type " type))
+                     (async/close! socket)))
+                 (recur)))
+             (log/debug "web socket closed")
+             (async/close! messages)
+             (async/close! replies))
 
     (reify
       p/ReadPort
-        (take! [_ handler]
-          (p/take! messages handler))
+      (take! [_ handler]
+        (p/take! messages handler))
 
       IErlangMBox
-        (self [_]
-          self)
+      (self [_]
+        self)
 
-        (close! [_]
-          (async/close! socket))
+      (close! [_]
+        (async/close! socket))
 
-        (call* [_ func params]
-          (go
-            (let [replies (async/chan) correlation (swap! correlation inc)
-                  module (namespace func) function (name func)]
-              (assert function "invalid function")
+      (ext-call* [_ func params info]
+        (go
+          (let [replies (async/chan) correlation (swap! correlation inc)
+                module (namespace func) function (name func)]
+            (assert function "invalid function")
 
-              (async/tap replies-mult replies)
-              (>! socket
+            (async/tap replies-mult replies)
+            (>! socket
+                (etf/encode [:call correlation [[(keyword (or module :erlang)) (keyword function)] (apply list params) info]]))
+
+            (loop []
+              (when-let [reply (<! replies)]
+                (let [[reply-correlation return] reply]
+                  (if (= correlation reply-correlation)     ; TODO: timeout handling
+                    (do
+                      (async/untap replies-mult replies)
+                      ; Protection from deadlocks.
+                      ; Look at "https://dev.clojure.org/jira/browse/ASYNC-58" for details.
+                      (go-loop []
+                               (if (some? (<! replies))
+                                 (recur)))
+                      return)
+                    (recur))))))))
+      (call* [_ func params]
+        (go
+          (let [replies (async/chan) correlation (swap! correlation inc)
+                module (namespace func) function (name func)]
+            (assert function "invalid function")
+
+            (async/tap replies-mult replies)
+            (>! socket
                 (etf/encode [:call correlation [(keyword (or module :erlang)) (keyword function)] (apply list params)]))
 
-              (loop []
-                (when-let [reply (<! replies)]
-                  (let [[reply-correlation return] reply]
-                    (if (= correlation reply-correlation) ; TODO: timeout handling
-                      (do
-                        (async/untap replies-mult replies)
-                        ; Protection from deadlocks.
-                        ; Look at "https://dev.clojure.org/jira/browse/ASYNC-58" for details.
-                        (go-loop []
-                          (if (some? (<! replies))
-                            (recur)))
-                        return)
-                      (recur))))))))
-
-        (send! [_ pid message]
-          (let [encoded (etf/encode [:send pid message])]
-            (go
-              (>! socket encoded)))))))
+            (loop []
+              (when-let [reply (<! replies)]
+                (let [[reply-correlation return] reply]
+                  (if (= correlation reply-correlation)     ; TODO: timeout handling
+                    (do
+                      (async/untap replies-mult replies)
+                      ; Protection from deadlocks.
+                      ; Look at "https://dev.clojure.org/jira/browse/ASYNC-58" for details.
+                      (go-loop []
+                               (if (some? (<! replies))
+                                 (recur)))
+                      return)
+                    (recur))))))))
+      (send! [_ pid message]
+        (let [encoded (etf/encode [:send pid message])]
+          (go
+            (>! socket encoded)))))))
 
 (defn mbox [url]
   (go
@@ -89,7 +112,7 @@
 
 (defn string-to-list [s]
   (apply list
-    (reduce
-      (fn [acc ix]
-        (conj acc
-          (.charCodeAt s ix))) [] (range (count s)))))
+         (reduce
+           (fn [acc ix]
+             (conj acc
+                   (.charCodeAt s ix))) [] (range (count s)))))
